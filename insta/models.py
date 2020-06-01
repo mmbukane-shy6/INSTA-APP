061b364784885386dee0f3b97fb42e32f14d70e0
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-01 10:30:41 +0300

    added styles.css

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='users/', default='user.png')
+    bio = models.TextField(default="Welcome!")
+
+    def __str__(self):
+        return f'{self.user.username} Profile' 
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    image_name = models.CharField(max_length = 30)
+    caption = models.TextField(default="Welcome Me!")
+    image = models.ImageField(upload_to='posts/')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, related_name= 'likes', blank = True)
+    
+    def total_likes(self):
+        self.likes.count()
+
+    def __str__(self):
+        return self.image_name
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.comment
--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import Post,Comment,Profile
 
 # Create your tests here.
+class Post(TestCase):
+ #set up method
+    def setUp(self):
+        self.caption= Post(location='caption')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.caption,Post))
+
+    def tearDown(self):
+        Post.objects.all().delete()
+
+    def test_save_method(self):
+        self.caption.save_location()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+
+    def test_delete_method(self):
+        self.caption.delete_location('caption')
+        post = Post.objects.all()
+        self.assertTrue(len(post)==0)
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+       self.profile=Profile()
+       self.profile.save_profile()
+    def test_instance(self):
+       self.assertTrue(isinstance(self.profile,Profile))
+    def test_save_profile(self):
+       self.profile.save_profile()
+       profiles = Profile.objects.all()
+       self.assertTrue(len(profiles) > 0)
+    def test_delete_profile(self):
+       self.profile.delete_profile()
+       profile = Profile.objects.all()
+       self.assertTrue(len(profile) == 0)
+class CommentTestClass(TestCase):
+    def setUp(self):
+       self.comment=Comment()
+       self.comment.save_comment()
+    def test_instance(self):
+       self.assertTrue(isinstance(self.comment,Comment))
+    def test_save_comment(self):
+       self.comment.save_comment()
+       comments = Comment.objects.all()
+       self.assertTrue(len(comments) > 0)
+    def test_delete_comment(self):
+       self.comment.delete_comment()
+       comment = Comment.objects.all()
+       self.assertTrue(len(comment) == 0)
