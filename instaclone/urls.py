6641012c665fe8ded67f99e86ee1b2f91c4f77dd
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-04 16:55:13 +0300

    all commits

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,22 +1,19 @@
-"""instaclone URL Configuration
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+urlpatterns=[
+    url(r'^$',views.timeline,name='timeline'),
+    url(r'^search/', views.search_results, name = 'search_results'),
+    url(r'^explore/', views.explore, name = 'explore'),
+    url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
+    url(r'^new/post/', views.new_post, name = 'new-post'),
+    url(r'^accounts/edit-profile/', views.edit_profile, name = 'edit-profile'),
+
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
